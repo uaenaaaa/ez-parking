d0ae7ddd939e0411e99af38e67c261456b52a65b
uaenaaaa <61849282+uaenaaaa@users.noreply.github.com>
2024-11-25 19:32:40 +0800

    ADJUST CONFIGURATION

--- vite.config.ts
+++ vite.config.ts
@@ -3,19 +3,25 @@ import { defineConfig } from 'vite';
 import fs from 'fs';
 import path from 'path';
 
-export default defineConfig({
-	plugins: [sveltekit()],
-	server: {
-		https: {
-			key: fs.readFileSync(path.resolve(__dirname, './certificates/localhost-key.pem')),
-			cert: fs.readFileSync(path.resolve(__dirname, './certificates/localhost.pem'))
-		},
-		proxy: {
-			'/api': {
-				target: 'https://localhost:5000',
-				secure: false,
-				changeOrigin: true
-			}
-		}
-	}
+export default defineConfig(({ mode }) => {
+	const isDevelopment = mode === 'development'; // Check if running in development mode
+
+	return {
+		plugins: [sveltekit()],
+		server: isDevelopment
+			? {
+					https: {
+						key: fs.readFileSync(path.resolve(__dirname, './certificates/localhost-key.pem')),
+						cert: fs.readFileSync(path.resolve(__dirname, './certificates/localhost.pem'))
+					},
+					proxy: {
+						'/api': {
+							target: 'https://localhost:5000',
+							secure: false,
+							changeOrigin: true
+						}
+					}
+				}
+			: undefined // No `server` config in production
+	};
 });
