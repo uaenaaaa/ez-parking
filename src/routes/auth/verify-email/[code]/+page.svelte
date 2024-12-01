88feb98dba1ba5c2584ac8ae2e82ea4f093407a6
uaenaaaa <61849282+uaenaaaa@users.noreply.github.com>
2024-12-01 11:14:38 +0800

    add email verification logic and loading indicator to verification page

---
+++ src/routes/auth/verify-email/[code]/+page.svelte
@@ -0,0 +1,16 @@
+<main class="flex min-h-screen w-full flex-col items-center justify-center gap-3">
+	<svg
+		class="-ml-1 mr-3 h-5 w-5 animate-spin text-white"
+		xmlns="http://www.w3.org/2000/svg"
+		fill="none"
+		viewBox="0 0 24 24"
+	>
+		<circle class="opacity-25" cx="12" cy="12" r="10" stroke="#000000" stroke-width="4"></circle>
+		<path
+			class="opacity-75"
+			fill="#000000"
+			d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+		></path>
+	</svg>
+	<p>Please wait...</p>
+</main>
